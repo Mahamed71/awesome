d37b047aeacf29bcaa82fef78aae66c4474e8356
Mahmod <Mahmoud@myndlift.com>
2018-05-03 13:23:20 +0300

    Add a new lib

--- app/src/main/java/myndlift/com/myndolibo/MainActivity.java
+++ app/src/main/java/myndlift/com/myndolibo/MainActivity.java
@@ -3,11 +3,14 @@ package myndlift.com.myndolibo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import myndlift.com.mylib.EscanorSan;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EscanorSan.log("Yes");
     }
 }
---
+++ mylib/src/main/java/myndlift/com/mylib/EscanorSan.java
@@ -0,0 +1,13 @@
+package myndlift.com.mylib;
+
+import android.util.Log;
+
+/**
+ * Created by mahmodmahamed on 03/05/2018.
+ */
+
+public class EscanorSan {
+    public  static void log(String text){
+        Log.v("EscanorSan",text);
+    }
+}
